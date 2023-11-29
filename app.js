6f3c9d2d1d2020d5ad563f51f04e0987ebcb6b3e
Faisal <md.faisal1899@yahoo.com>
2023-11-29 12:57:13 +0600

    double binding the form

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { loadData } from './service/Menu.js';
 import Router from './service/Router.js';
 import Store from './service/Store.js';
 
-import './components/CartItem.js';
+// import './components/CartItem.js';
 import './components/DetailsPage.js';
 import './components/MenuPage.js';
 import './components/OrderPage.js';

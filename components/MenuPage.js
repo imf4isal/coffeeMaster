ea83d76a582b6299d4a9e4ae052d9a2174b14689
Faisal <md.faisal1899@yahoo.com>
2023-11-29 11:23:56 +0600

    render menu item

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -21,6 +21,31 @@ export default class MenuPage extends HTMLElement {
     const template = document.getElementById('menu-page-template');
     const content = template.content.cloneNode(true);
     this.root.appendChild(content);
+
+    window.addEventListener('appmenuchange', () => {
+      this.render();
+    });
+
+    this.render();
+  }
+
+  render() {
+    if (app.store.menu) {
+      this.root.querySelector('#menu').innerHTML = '';
+
+      for (let category of app.store.menu) {
+        const liCategory = document.createElement('li');
+        liCategory.innerHTML = `
+          <h3>${category.name}</h3>
+          <ul class='category'>
+          </ul>
+        `;
+
+        this.root.querySelector('#menu').appendChild(liCategory);
+      }
+    } else {
+      this.root.querySelector('#menu').innerHTML = 'loading..';
+    }
   }
 }
 
